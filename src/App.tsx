2590ec097f02464cb7002e79a603517c9cc7e506
Tatsuya Kusakabe <t.kskb.boyohnbounce@gmail.com>
2019-03-24 02:28:24 +0900

    Removing unused locals

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { default as React, Component, Fragment } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import { default as Landing } from './components/landing';
+import { default as React, Fragment } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Landing } from './components/landing';
 
 export const App = () => (
   <BrowserRouter>
@@ -8,18 +8,4 @@ export const App = () => (
       <Route exact path="/" component={Landing} />
     </Fragment>
   </BrowserRouter>
-);
-
-const About = () => (
-  <Fragment>
-    <h2>About</h2>
-    <p>フレンズに投票するページです</p>
-  </Fragment>
-);
-
-const Friends = () => (
-  <Fragment>
-    <h2>Friends</h2>
-    <p>ここにフレンズのリストを書きます</p>
-  </Fragment>
 );
