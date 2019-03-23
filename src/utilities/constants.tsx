5d39548f8bf17ba422de256632e6f59bd27816fb
Tatsuya Kusakabe <t.kskb.boyohnbounce@gmail.com>
2019-03-24 02:20:29 +0900

    Defining time difference func

--- src/utilities/constants.tsx
+++ src/utilities/constants.tsx
@@ -8,3 +8,23 @@ export const Host = {
   server: { dev: 'http://localhost:3001' },
   client: { dev: 'http://localhost:3000' },
 };
+
+export const TimeDiff = (datePosted: string): string => {
+  const datePostedObj: Date = new Date(datePosted);
+  const timestampNow: number = Date.now();
+  const timestampPosted: number = Date.parse(datePosted);
+  const timeDiff = timestampNow - timestampPosted;
+
+  const unitSecond = 1000;
+  const unitMinute = 1000 * 60;
+  const unitHour = 1000 * 60 * 60;
+  const unitDay = 1000 * 60 * 60 * 24;
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  return (timeDiff < 60 * unitSecond) ? `${Math.floor(timeDiff / unitSecond)}s`
+    : (timeDiff < 60 * unitMinute) ? `${Math.floor(timeDiff / unitMinute)}m`
+    : (timeDiff < 24 * unitHour) ? `${Math.floor(timeDiff / unitHour)}h`
+    : (timeDiff < 7 * unitDay) ? `${Math.floor(timeDiff / unitDay)}d`
+    : `${months[datePostedObj.getUTCMonth()]} ${datePostedObj.getUTCMonth()}`;
+};
