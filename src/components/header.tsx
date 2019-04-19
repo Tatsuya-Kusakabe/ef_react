07fd01dcc0c7cbe9cde1fd7ef8bf687d597cc047
Tatsuya Kusakabe <t.kskb.boyohnbounce@gmail.com>
2019-04-19 23:15:29 +0900

    Adding flash messages to header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,43 @@
 import { default as React, Component } from 'react';
 import { default as styled } from 'styled-components';
 import changeCase from 'change-case';
+import { default as FlashStore } from '../stores/flash';
 
 interface Props {}
 interface State {
   openedTab: string;
+  flash: (string | null);
 }
 
 export class Header extends Component<Props, State> {
+  onStoreChangeHandler: () => void;
+
   constructor(props: Props) {
     super(props);
     this.state = this.initialState;
+    this.onStoreChangeHandler = this.onStoreChange.bind(this);
   }
 
   get initialState() {
-    return { openedTab: 'home' };
+    return { openedTab: 'home', flash: null };
+  }
+
+  componentDidMount() {
+    FlashStore.onChange(this.onStoreChangeHandler);
+  }
+
+  componentWillUnmount() {
+    FlashStore.offChange(this.onStoreChangeHandler);
+  }
+
+  onStoreChange() {
+    const flash = FlashStore.getFlash('auth');
+    this.setState({ flash });
+
+    // レンダリング毎の表示は鬱陶しいので、componentDidUpdate() での alert は却下
+    // prevState との比較は、同じミスを繰り返した時に表示できないので、componentDidMount(...prev) での alert は却下
+    // onSubmit() 中では state の更新が間に合わないので、onSubmit() での alert は却下
+    if (flash) alert(flash);
   }
 
   onButtonClick(e: string) {
