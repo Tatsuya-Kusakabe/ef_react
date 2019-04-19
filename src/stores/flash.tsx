86a286df00d8b8b5fa236d5c5c5dbc9e86596023
Tatsuya Kusakabe <t.kskb.boyohnbounce@gmail.com>
2019-04-19 23:13:43 +0900

    Initializing flash store

---
+++ src/stores/flash.tsx
@@ -0,0 +1,54 @@
+import { Dispatcher } from '../dispatcher';
+import { ActionTypes } from '../utilities/constants';
+import { Action, Flash, FlashKeys } from '../utilities/types';
+import { BaseStore } from '../utilities/base_store';
+
+class BaseFlashStore extends BaseStore {
+  getFlashesObj(): Flash[] {
+    if (!this.get('flash')) this.set('flash', []);
+    return this.get('flash');
+  }
+
+  getFlash(key: FlashKeys): (string | null) {
+    if (!this.getFlashesObj().length) return null;
+
+    const focusedFlash = this.getFlashesObj().find((selfFlash: Flash) => {
+      return Object.keys(selfFlash)[0] === `${key}`;
+    });
+
+    return (!!focusedFlash) ? focusedFlash[key] : null;
+  }
+
+  setFlash(passedFlash: Flash): void {
+    if (!this.getFlashesObj().length) this.set('flash', [passedFlash]);
+
+    const updatedFlashesObj = this.getFlashesObj().map((selfFlash: Flash) => {
+      return (Object.keys(selfFlash)[0] === Object.keys(passedFlash)[0])
+        ? passedFlash : selfFlash;
+    });
+
+    this.set('flash', updatedFlashesObj);
+  }
+}
+
+const FlashStore = new BaseFlashStore();
+
+FlashStore.dispatchToken = Dispatcher.register((payload: any) => {
+  const action: Action = payload.action;
+
+  switch (action.type) {
+    case ActionTypes.AUTH__SIGN_UP:
+    case ActionTypes.AUTH__SIGN_IN:
+    case ActionTypes.AUTH__SIGN_OUT:
+    case ActionTypes.FLASH__SET_FLASH:
+      FlashStore.setFlash(action.message);
+      FlashStore.emitChange();
+      break;
+
+    default:
+  }
+
+  return true;
+});
+
+export default FlashStore;
