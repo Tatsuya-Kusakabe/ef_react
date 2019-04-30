1dc8c9483e36aabe9a529f4f0dc3029b84df3288
Tatsuya Kusakabe <t.kskb.boyohnbounce@gmail.com>
2019-04-30 11:16:57 +0900

    Making volatile props.history

--- utilities/history.ts
+++ utilities/history.ts
@@ -1,3 +1,8 @@
 import { createBrowserHistory } from 'history';
 
-export default createBrowserHistory();
+// ページをリロードしたら history.location.state を初期化する
+// (https://stackoverflow.com/questions/47512104)
+const history = createBrowserHistory();
+if (history.location.state) history.replace({ ...history.location, state: {} });
+
+export default history;
