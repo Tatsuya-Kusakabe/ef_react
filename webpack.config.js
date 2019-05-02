d4edcee557cf3057a48aea87957a38b62616b119
Tatsuya Kusakabe <t.kskb.boyohnbounce@gmail.com>
2019-05-02 22:45:05 +0900

    Creating output files when reloaded

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,8 @@ module.exports = {
 
   // webpack 4 のデフォルト値 ... context: views, entry: views/index.tsx, output: dist/main.js
   // views/index.tsx を起点にビルドし、結果は dist/bundle.js に出力
+  // webpack --watch を指定しないと、変更時に dist/bundle.js が更新されない
+  // (https://stackoverflow.com/questions/41928358/)
   entry: './views/index.tsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -59,7 +61,10 @@ module.exports = {
       test: /\.tsx?$/,              // xxx.ts, xxx.tsx に対して
       loader: 'tslint-loader',      // tslint-loader を使って構文チェック
       exclude: [/node_modules/],
-      options: { emitErrors: true } // TSLint が出してくれた警告をエラーとして扱ってくれる
+      options: {
+        emitErrors: true, // TSLint が出してくれた警告をエラーとして扱ってくれる
+        typeCheck: true   // warning を消す (https://qiita.com/kurosame/items/3c28f45c8b2e65f5c69d)
+      }
     }, {
       test: /\.tsx?$/,                         // xxx.ts, xxx.tsx に対して
       loader: 'ts-loader',                     // ts-loader を使ってトランスパイル
